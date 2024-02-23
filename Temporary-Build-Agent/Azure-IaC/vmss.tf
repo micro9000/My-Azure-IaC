345102e4506aa71822162fbd71215821c91fdfb4
Raniel Garcia <ranielgarcia2596@gmail.com>
2024-02-23 21:03:11 +0800

    Update resource name

--- Temporary-Build-Agent/Azure-IaC/vmss.tf
+++ Temporary-Build-Agent/Azure-IaC/vmss.tf
@@ -1,28 +1,29 @@
 locals {
   first_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDNswICCIklyE4C5Q8rNMCYgQ3oHBOkD2SZUIzTlT0OfIJ8bPf35EW4OzMEsq7ZFlx8YHs8RHbcut+5jC8hDFubYX2QnVpTYNVIHBhUkItUtdZX87vMy9oSocssCF/xr4w9lOQgLtbKWG2IjRWL3Vy711jsFaGD+WDDf7K5D48nDoR+AI/cLyJLyFRe0XXvTuz/57ZEw9XCYASKUUdJ6tXpszexQzYuDOjrLw7yzT2+phnS1oOYjU9k/nvdxQXmaok8NMXHpFrIwl07P7dUEQ3Q9tvN33gbUPMRP91epxETN24k4QlTsyFzwrnyzI2o1/o6h95jnBQyjXb9qb1Nso17SuC8PeBkJhXvJkM65lAotsR0vBclV6ni4mIl3oYIhKihyNVgFzdqz7M8FR9YxXiH6dGXyqn17unx9fvLY2tIiTlfPx+QVl3I2C0t2+f77WT1aSq2BIl0tc7+LLyae1RedjxxSqUX0/6vTHhQh52FGFjeCP2ZLb3/9GvqFrrdDavt5dS1v8JkBfREGZNgychMhPiv+TCwV3/fNmVuhiP/sAJsNSe9r8hCwa8N8YEi105lF1jAgXSLpunA7I4y3whMVJHET1y9U2QR/dGDQLS583l6uBr4fqD+iLk3+yLGHnQcuTbxFMRKoB7xuedBBhyr1IM8/VIqRWSwivt2p7ZSnw== ranie@raniel-garcia"
+  prefix = "temp-agent"
 }
 
 resource "azurerm_resource_group" "build-agent" {
-  name     = "build-agent-resources"
+  name     = "${local.prefix}-resources"
   location = "East Asia"
 }
 
 resource "azurerm_virtual_network" "build-agent" {
-  name                = "build-agent-network"
+  name                = "${local.prefix}-network"
   resource_group_name = azurerm_resource_group.build-agent.name
   location            = azurerm_resource_group.build-agent.location
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "internal" {
-  name                 = "internal"
+  name                 = "${local.prefix}-internal"
   resource_group_name  = azurerm_resource_group.build-agent.name
   virtual_network_name = azurerm_virtual_network.build-agent.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_linux_virtual_machine_scale_set" "build-agent" {
-  name                = "build-agent-vmss"
+  name                = "${local.prefix}-vmss"
   resource_group_name = azurerm_resource_group.build-agent.name
   location            = azurerm_resource_group.build-agent.location
   sku                 = "Standard_F2"
@@ -47,11 +48,11 @@ resource "azurerm_linux_virtual_machine_scale_set" "build-agent" {
   }
 
   network_interface {
-    name    = "build-agent"
+    name    = "${local.prefix}-interface"
     primary = true
 
     ip_configuration {
-      name      = "internal"
+      name      = "${local.prefix}-internal"
       primary   = true
       subnet_id = azurerm_subnet.internal.id
     }
@@ -59,7 +60,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "build-agent" {
 }
 
 resource "azurerm_virtual_machine_scale_set_extension" "build-agent" {
-  name                         = "build-agent"
+  name                         = "${local.prefix}-vmss-extension"
   virtual_machine_scale_set_id = azurerm_linux_virtual_machine_scale_set.build-agent.id
   publisher                    = "Microsoft.Azure.Extensions"
   type                         = "CustomScript"
